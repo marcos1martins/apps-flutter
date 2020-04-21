8933f60f649b987fd5557400cdb5173822b66c44
MARCOS MARTINS <marcosm.caixa@gmail.com>
2020-04-20 21:33:25 -0300

    finalização

---
+++ mobx_tutorial/lib/counter.dart
@@ -0,0 +1,17 @@
+
+import 'package:mobx/mobx.dart';
+
+part 'counter.g.dart';
+
+class Counter = _Counter with _$Counter;
+
+abstract class _Counter with Store {
+
+  @observable
+  int count = 0;
+
+  @action
+  void increment(){
+    count++;
+  }
+} 
--- mobx_tutorial/lib/main.dart
+++ mobx_tutorial/lib/main.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'counter.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,10 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Mobx Tutorial'),
     );
   }
 }
@@ -28,13 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Counter counter = Counter();
 
   @override
   Widget build(BuildContext context) {
@@ -44,24 +39,27 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: counter.increment,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
